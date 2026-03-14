abea04d2e73af0c6aa1c2d7f0987da6e5df7177b
dongso <ehdth5806@naver.com>
2026-03-14 19:16:18 +0900

    두개의합계_0 복습

--- LeetCode/src/_1_50/Main.java
+++ LeetCode/src/_1_50/Main.java
@@ -1,10 +1,38 @@
 package _1_50;
 
-public class Main {
+import _1_50._01_두개의합계_0.Solution;
 
+public class Main {
+	
+	static class Solution {
+		
+		public int[] twoSum(int[] nums, int target) {
+			
+			int leng = nums.length;
+			
+			for(int i = 0; i < leng - 1; i++) {
+				for(int j = i + 1; i < leng; j++) {
+					if(target == nums[i] + nums[j]) {
+						return new int[] {i, j};
+					}
+				}
+			}
+			
+			return new int[] {};
+		}
+		
+	}
+	
 	public static void main(String[] args) {
 		
+		Solution sol = new Solution();
 		
+		int[] nums = {2, 7, 11, 15};
+		int target = 9;
+		
+		int[] result = sol.twoSum(nums, target);
+		
+		System.out.println(result[0] + ", " + result[1]);
 	}
 
 }
