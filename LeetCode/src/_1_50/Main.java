f40ad9f03abce05fd1d877429d5317b915115010
dongso <ehdth5806@naver.com>
2026-03-15 20:17:17 +0900

    두숫자를더하세요 복습 > 다음시간은 중복문자가없는가장긴부분문자열 복습진행

--- LeetCode/src/_1_50/Main.java
+++ LeetCode/src/_1_50/Main.java
@@ -1,26 +1,53 @@
 package _1_50;
 
-import java.util.HashMap; 
-import java.util.Map;
-
 public class Main {
+	
+	// ListNode 정의
+    static class ListNode {
+        int val; // 노드의 실제 값
+        ListNode next; // 노드와 연결 돼 있는 노드의 참조주소를 저장하는 변수
+
+        // 노드의 실제 값을 받는 생성자
+        ListNode(int val) {
+            this.val = val;
+        }
+        
+        // 노드의 실제 값과, 다른노드의 참조주소를 받는 생성자
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
 	
 	static class Solution {
 		
-		public int[] twoSum(int[] nums, int target) {
-			Map<Integer, Integer> map = new HashMap<Integer, Integer>();
-			
-        	for (int i = 0; i < nums.length; i++) {
-        		int complement = target - nums[i];
-        		if (map.containsKey(complement)) { // target에서 뺀 값이 배열에 있다면
-                    return new int[] { map.get(complement), i }; // 뺀값에 해당하는 인덱스, i의값(index) 2개를 반환
-                }
-        		map.put(nums[i], i);
-        	}
-        	
-        	return new int[] {};
-			
-		}
+		 public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+			 
+			 ListNode dummyHead = new ListNode(0);
+			 
+			 ListNode curr = dummyHead;
+			 
+			 int carry = 0;
+			 
+			 while(l1 != null || l2 != null || carry != 0) {
+				 int x = (l1 != null) ? l1.val : 0;
+				 int y = (l2 != null) ? l2.val : 0;
+				 
+				 int sum = x + y + carry;
+				 
+				 carry = sum / 10;
+				 
+				 curr.next = new ListNode(sum % 10);
+				 
+				 curr = curr.next;
+				 
+				 if(l1 != null) l1 = l1.next;
+				 if(l2 != null) l2 = l2.next;
+				 
+			 }
+			 
+			 return dummyHead.next;
+		 }
 		
 	}
 	
@@ -28,12 +55,19 @@ public class Main {
 		
 		Solution sol = new Solution();
 		
-		int[] nums = {2, 7, 11, 15};
-		int target = 9;
+		ListNode l1 = new ListNode(2);
+		l1.next = new ListNode(4);
+		l1.next.next = new ListNode(3);
 		
-		int[] result = sol.twoSum(nums, target);
+		ListNode l2 = new ListNode(5);
+		l2.next = new ListNode(6);
+		l2.next.next = new ListNode(4);
 		
-		System.out.println(result[0] + ", " + result[1]);
+		ListNode result = sol.addTwoNumbers(l1, l2);
+		
+		while(result != null) {
+			System.out.print(result.val + " ");
+			result = result.next;
+		}
 	}
-
 }
